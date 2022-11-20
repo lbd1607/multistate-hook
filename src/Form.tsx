b56f582f01287d29844aab54caad326eb7e00d8d
lbd1607 <35612977+lbd1607@users.noreply.github.com>
2022-11-20 16:31:17 -0600

    Handle submit

--- src/Form.tsx
+++ src/Form.tsx
@@ -66,6 +66,10 @@ const Form = () => {
     }
   };
 
+  const handleSubmit = () => {
+    handleReset();
+  };
+
   const handleSetOption = ({
     key,
     selectedValue,
@@ -83,75 +87,78 @@ const Form = () => {
   console.table(state); //See the values as they change
 
   return (
-    <div className="form-wrapper">
-      <h1>Sandwich order form</h1>
-      <SearchableDropdown
-        id={categories.breads.fieldLabel}
-        fieldLabel={categories.breads.fieldLabel}
-        options={categories.breads.options}
-        onChange={(selected: ValueLabelPair) =>
-          handleSetOption({ key: "breads", selectedValue: selected })
-        }
-        value={state.breads}
-      />
-      <SearchableDropdown
-        id={categories.meats.fieldLabel}
-        fieldLabel={categories.meats.fieldLabel}
-        options={categories.meats.options}
-        onChange={(selected: ValueLabelPair) =>
-          handleSetOption({ key: "meats", selectedValue: selected })
-        }
-        value={state.meats}
-        isDisabled={state.plantBasedEntrees.value !== ""}
-      />
-      <SearchableDropdown
-        id={categories.plantBasedEntrees.fieldLabel}
-        fieldLabel={categories.plantBasedEntrees.fieldLabel}
-        options={categories.plantBasedEntrees.options}
-        onChange={(selected: ValueLabelPair) =>
-          handleSetOption({
-            key: "plantBasedEntrees",
-            selectedValue: selected,
-          })
-        }
-        value={state.plantBasedEntrees}
-        isDisabled={state.meats.value !== ""}
-      />
-      <SearchableDropdown
-        id={categories.cheeses.fieldLabel}
-        fieldLabel={categories.cheeses.fieldLabel}
-        options={categories.cheeses.options}
-        onChange={(selected: ValueLabelPair) =>
-          handleSetOption({ key: "cheeses", selectedValue: selected })
-        }
-        value={state.cheeses}
-        isMulti={true}
-      />
-      <SearchableDropdown
-        id={categories.vegetables.fieldLabel}
-        fieldLabel={categories.vegetables.fieldLabel}
-        options={categories.vegetables.options}
-        onChange={(selected: ValueLabelPair) =>
-          handleSetOption({ key: "vegetables", selectedValue: selected })
-        }
-        value={state.vegetables}
-        isMulti={true}
-      />
-      <TextInput
-        id={"name"}
-        label={"Name"}
-        value={state.name}
-        onChange={(e: any) => handleSetName(e.target.value)}
-      />
-      <div className="btn-wrapper">
-        <button onClick={handleReset} className="btn reset" type="reset">
-          Reset
-        </button>
-        <button className="btn submit" type="submit">
-          Submit
-        </button>
-      </div>{" "}
-    </div>
+    <form>
+      <div className="form-wrapper">
+        <h1>Sandwich order form</h1>
+        <SearchableDropdown
+          id={categories.breads.fieldLabel}
+          fieldLabel={categories.breads.fieldLabel}
+          options={categories.breads.options}
+          onChange={(selected: ValueLabelPair) =>
+            handleSetOption({ key: "breads", selectedValue: selected })
+          }
+          value={state.breads}
+        />
+        <SearchableDropdown
+          id={categories.meats.fieldLabel}
+          fieldLabel={categories.meats.fieldLabel}
+          options={categories.meats.options}
+          onChange={(selected: ValueLabelPair) =>
+            handleSetOption({ key: "meats", selectedValue: selected })
+          }
+          value={state.meats}
+          isDisabled={state.plantBasedEntrees.value !== ""}
+        />
+        <SearchableDropdown
+          id={categories.plantBasedEntrees.fieldLabel}
+          fieldLabel={categories.plantBasedEntrees.fieldLabel}
+          options={categories.plantBasedEntrees.options}
+          onChange={(selected: ValueLabelPair) =>
+            handleSetOption({
+              key: "plantBasedEntrees",
+              selectedValue: selected,
+            })
+          }
+          value={state.plantBasedEntrees}
+          isDisabled={state.meats.value !== ""}
+        />
+        <SearchableDropdown
+          id={categories.cheeses.fieldLabel}
+          fieldLabel={categories.cheeses.fieldLabel}
+          options={categories.cheeses.options}
+          onChange={(selected: ValueLabelPair) =>
+            handleSetOption({ key: "cheeses", selectedValue: selected })
+          }
+          value={state.cheeses}
+          isMulti={true}
+        />
+        <SearchableDropdown
+          id={categories.vegetables.fieldLabel}
+          fieldLabel={categories.vegetables.fieldLabel}
+          options={categories.vegetables.options}
+          onChange={(selected: ValueLabelPair) =>
+            handleSetOption({ key: "vegetables", selectedValue: selected })
+          }
+          value={state.vegetables}
+          isMulti={true}
+        />
+        <TextInput
+          id={"name"}
+          label={"Name"}
+          value={state.name}
+          onChange={(e: any) => handleSetName(e.target.value)}
+          isRequired={true}
+        />
+        <div className="btn-wrapper">
+          <button onClick={handleReset} className="btn reset" type="reset">
+            Reset
+          </button>
+          <button onClick={handleSubmit} className="btn submit" type="submit">
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
   );
 };
 
